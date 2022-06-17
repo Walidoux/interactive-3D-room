3e748587fb0808b7d84a8db8c0ff7eaf57fa0288
Walidoux <ma.walidkorchi@icloud.com>
2022-06-17 02:34:56 +0100

    refactor: update & improve code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Component, createSignal } from "solid-js";
-import "./styles/main.scss";
+import { Component, createSignal } from 'solid-js'
+
+import { Scene } from './components/Scene/Scene'
+import './styles/main.scss'
 
 const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 1);
+  const [counter, setCounter] = createSignal(0)
+  setInterval(setCounter, 1000, (c: number) => c + 1)
 
-  return <h1 class="header">{counter()}</h1>;
-};
+  return (
+    <h1 class='header'>
+      {counter()}
+      <Scene />
+    </h1>
+  )
+}
 
-export default App;
+export default App
