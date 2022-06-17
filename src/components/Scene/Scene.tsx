e61c2bcb4fe43033252be4196dd2285fc0de399e
Walidoux <ma.walidkorchi@icloud.com>
2022-06-17 02:42:25 +0100

    refactor: add intellisense for env variables

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,18 +1,14 @@
 import { Application } from '@splinetool/runtime'
 import { Component, onMount } from 'solid-js'
 
-import styles, { scene } from './Scene.module.scss'
+import styles from './Scene.module.scss'
 
 export const Scene: Component = () => {
   const canvas = <canvas class={styles.scene} />
 
-  console.log(scene)
-
   onMount(() => {
     const scene = new Application(canvas as HTMLCanvasElement)
-    scene
-      .load('https://prod.spline.design/u8ay3m2mAC8tiC3Z/scene.splinecode')
-      .catch((error) => error)
+    scene.load(process.env.SPLINE_SCENE).catch((error) => error)
   })
 
   return canvas
